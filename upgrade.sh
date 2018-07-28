47a3611615bbdd1db57ca52f06d3ac638e1bab36
syedhumair <syed.humair@gmail.com>
2018-07-28 06:51:48 -0400

    Added upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Please stop home assistant process using pi user"
+echo "Command: sudo systemctl stop home-assistant@homeassistant"
+echo "Press any key to continue"
+read a
+source /srv/homeassistant/bin/activate
+python3 -m pip install --upgrade homeassistant
+
+echo "Please start home assistant process using pi user"
+echo "Command: sudo systemctl start home-assistant@homeassistant"
